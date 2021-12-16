01dc345d56b23e618931738ff569bb0ed6d1b98a
jongwon <wonwonkk@gmail.com>
2021-12-16 10:25:32 +0900

    gateway init

---
+++ gateway-service/src/main/java/com/example/gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,32 @@
+package com.example.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
+    public JwtAuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config {
+
+    }
+}
